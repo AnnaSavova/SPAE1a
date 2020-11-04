a796b49e527730171eac4d2e8305e922b3910ef2
Anna Savova <59999314+AnnaSavova@users.noreply.github.com>
2020-11-04 21:17:48 +0000

    added spae1 files for testing
    Added the spaae1 files to facilitate testing on stlinux

---
+++ date.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include "date.h"
+
+struct date {
+        int day;
+        int month;
+        int year;
+};
+
+Date *date_create(char *datestr){
+        int day = 0;
+        int month = 0;
+        int year = 0;
+
+        /* check datestr is in the expected dd/mm/yyyy format and assign the information to the ints day,month and year respectively*/
+        sscanf(*datestr,"%2d/%2d/%4d",&day,&month,&year);
+
+        if (day >= 1 && day<= 31 && month >= 1 && month <= 12){
+            Date *d = malloc(sizeof(Date));
+            d -> day = day;
+            d -> month = month;
+            d -> year = year;
+            return d;
+        } else {
+            return NULL;
+        }
+}
+
+Date *date_duplicate(Date *d){
+    Date *dupe = malloc(sizeof(Date));
+    if (d == NULL || dupe == NULL) {
+        return NULL;
+    } else {
+        dupe = d;
+        return dupe;
+    }
+}
+
+int date_compare(Date *date1, Date *date2){
+
+
+    if (date1.year<date2.year || (date1.year>=date2.year && date1.month<date2.month) || (date1.year>=date2.year && day1.month>=day2.month && date1.day<date2.day)){
+        return -999;
+    } else if (date1.year == date2.year && date1.month == date2.month && date1.day == date2.day){
+        return 0;
+    } else {
+        return 999;
+    }
+}
+
+void date_destroy(Date *d){
+    free(d);
+}
---
+++ tldlist.c
@@ -0,0 +1,260 @@
+#include "tdlist.h"
+#include "date.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+struct tldnode{
+    char *host;
+    struct tldnode *left;
+    struct tldnode *right;
+    struct tldnode *parent;
+    long count;
+    int height;
+    int balance;
+};
+
+struct tldlist{
+    TLDNode *root;
+    Date *begin;
+    Date *end;
+    int count;
+};
+
+struct tlditerator{
+    TLDNode *curr_node;
+    TLDNode *next;
+    TLDNode *root;
+
+
+};
+
+// Start of helper functions:
+void reheight(TLDNode *node){
+    if (node != NULL){
+        curr_max_height = 0
+        if (node->left->height > node->right->height){
+            curr_max_height = node->left->height;
+        } else {
+            curr_max_height = node->right->height;
+        }
+        node->height = 1 + curr_max_height;
+    }
+}
+
+void setBalance(TLDNode *n){
+    reheight(n);
+    n->balance = (n->right->height - n->left->height);
+}
+
+TLDNode rotateLeft(TLDNode *a){
+    TLDNode *b = malloc(sizeof(TLDNode));
+    b = a->right;
+    b -> parent = a->parent;
+
+    a->right = b->left;
+    if (a->right != NULL){
+        a->right->parent = a;
+    }
+    b->left = a;
+    a->parent = b;
+
+    if (b->parent != NULL){
+        if (b->parent->right == a){
+            b->parent->right = b;
+        } else {
+            b->parent->left = b;
+        }
+    }
+
+    setBalance(a);
+    setBalance(b);
+
+    return b;
+}
+
+TLDNode rotateRight(TLDNode *a){
+    TLDNode *b = malloc(sizeof(TLDNode));
+    b = a->left;
+    b->parent = a->parent;
+
+    a->left = b->right;
+
+    if (a->left != NULL){
+        a->left->parent = a;
+    }
+
+    b->right = a;
+    a->parent = b;
+
+    if (b->parent != null){
+        if (b->parent->right == a){
+            b->parent->right = b;
+        } else {
+            b->parent->left = b;
+        }
+    }
+
+    setBalance(a);
+    setBalance(b);
+
+    return b;
+}
+
+TLDNode rotateLeftThenRight(TLDNode *n) {
+    n->left = rotateLeft(n->left);
+    return rotateRight(n);
+}
+
+TLDNode rotateRightThenLeft(TLDNode *n){
+    n->right = rotateRight;
+    return rotateLeft(n);
+}
+
+void rebalance(TLDNode *n){
+    setBalance(n);
+
+    if (n -> balance == -2){
+        if (n->left ->left->height >= n->left->right->height){
+            n = rotateRight(n)
+        }
+        else {
+            n = rotateLeftThenRight(n)
+        }
+    }
+}
+// End of helper functions
+
+TLDList tldlist_create(Date *begin, Date *end){
+
+    if (date_compare(begin,end) < 0){
+        TLDNode *root = NULL;
+        TLDList *new_list = malloc(sizeof(TLDList));
+        if (new_list == NULL){
+            return NULL;
+        } else {
+            new_list -> begin = begin;
+            new_list -> end = end;
+            new_list -> root = root;
+            new_list -> count = 0;
+            return new_list;
+        }
+    } else {
+        return NULL;
+    }
+}
+
+void tldlist_destroy(TLDList *tld){
+    free(tld);
+}
+
+int tldlist_add(TLDList *tld, char *hostname, Date *d){
+    char *host;
+    if (date_compare(tld.begin, d) < 0 && date_compare(tld.end, d) > 0){
+        char *host_orig = strrchr(hostname, ".")
+        if (host[0] == "."){
+            host = tolower(host_orig + 1);
+        } else {
+            host = tolower(host_orig);
+        }
+
+        if (tld.root == NULL) {
+            TLDNode *root_orig = malloc(sizeof(TLDNode));
+            root_orig -> host = host;
+            root_orig -> left = NULL;
+            root_orig -> right = NULL;
+            tld -> root = root_orig;
+            tld -> count += 1;
+        }
+
+        TLDNode *node = malloc(sizeof(TLDNode));
+        node = tld->root;
+        while (true){
+            if (strcmp(node.host,host) == 0) { return 0}
+
+            TLDNode *parent_node = malloc(sizeof(TLDNode));
+            parent_node = node;
+
+            bool goLeft = strcmp(node.host, host);
+            node = goLeft ? node.left : node.right;
+
+            if (node == NULL){
+                if (goLeft) {
+                    TLDNode *curr_node_left = malloc(sizeof(TLDNode));
+                    curr_node_left -> host = host;
+                    curr_node_left -> parent = parent_node;
+                    parent_node -> left = *current_node_left;
+                } else {
+                    TLDNode *curr_node_right = malloc(sizeof(TLDNode));
+                    curr_node_right -> host = host;
+                    curr_node_right -> parent = parent_node;
+                    parent_node -> right = *curr_node_right
+                }
+                tld -> count += 1;
+                rebalance(parent_node);
+                break;
+            }
+        }
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+long tldlist_count(TLDList *tld){
+    return tld -> count
+}
+
+TLDIterator *tldlist_iter_create(TLDList *tld){
+    TLDIterator *iter = malloc(sizeof(TLDIterator));
+    iter->root = tld->root;
+    iter->curr_node = tld->root;
+
+    if(iter != NULL){
+        return iter;
+    } else{
+        return NULL;
+    }
+}
+
+TLDIterator *tldlist_iter_next(TLDIterator *iter){
+    if (iter->curr_node->left != NULL){
+        return iter->curr_node->left;
+    } else if (iter->curr_node->right != NULL){
+        return = iter->curr_node->right;
+    } else {
+        return NULL;
+    }
+}
+
+void tldlist_iter_destroy(TLDIterator *iter){
+    free(iter);
+}
+
+char *tldnode_tldname(TLDNode *node){
+    return node->host;
+}
+
+// helper method for tldnode_count
+void tld_traverse(TLDNode *node, char *desired_host,long count){
+    if(node == NULL){
+        return;
+    }
+    if (strcmp(node->host,desired_host) == 0){
+        count += 1;
+        node->count = count;
+    }
+
+    tld_traverse(node->left, desired_host,count);
+    tld_traverse(node->right, desired_host, count);
+
+}
+// end of helper method
+long tldnode_count(TLDNode *node){
+    node->count = 0;
+    char *desired_host = node->host
+    tld_traverse(node, desired_host, 0);
+    node->count = count;
+    return node->count;
+}
+
